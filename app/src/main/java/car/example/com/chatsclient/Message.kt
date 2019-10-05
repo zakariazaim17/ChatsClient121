cbcc81bfa6960a463f36f04acbbccfc630fd9ca6
amanuela <babyzewdie@gmail.com>
2019-10-05 22:27:50 +0300

    first commit

---
+++ app/src/main/java/car/example/com/chatsclient/Message.kt
@@ -0,0 +1,10 @@
+package car.example.com.chatsclient
+
+class Message(var chatMsg:String, private val userName:String, var Time:String) {
+
+    //message structure
+    override fun toString(): String {
+        return "$chatMsg from $userName at $Time"
+    }
+
+}
