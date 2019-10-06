47ed21e507f28a97962d8cb80e9414887b508a2c
amanuela <babyzewdie@gmail.com>
2019-10-06 14:34:34 +0300

    latest commit

---
+++ app/src/main/java/car/example/com/chatsclient/ChatTab.kt
@@ -0,0 +1,12 @@
+package car.example.com.chatsclient
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChatTab : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat_tab)
+    }
+}
